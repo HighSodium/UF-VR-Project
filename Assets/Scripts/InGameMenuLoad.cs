da3bb25b4f0ef532a0bbab20e1b656ce2356d2f8
Bayron Najera <norybyte@gmail.com>
2024-08-07 18:33:22 -0400

    Updated InGame Menu
    Made ingame menu toggle open and close

--- Assets/Scripts/InGameMenuLoad.cs
+++ Assets/Scripts/InGameMenuLoad.cs
@@ -6,12 +6,13 @@ using UnityEngine.UI;
 public class InGameMenuLoad : MonoBehaviour
 {
     [Header("UI Pages")]
+    public GameObject startMainMenu;
     public GameObject mainMenu;
     public GameObject options;
     public GameObject about;
 
     [Header("Main Menu Buttons")]
-    public Button startButton;
+    public Button startMenuButton;
     public Button optionButton;
     public Button aboutButton;
     public Button exitButton;
@@ -21,14 +22,11 @@ public class InGameMenuLoad : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //EnableMainMenu();
+        startMenuButton.onClick.AddListener(EnableMainMenu);
 
-        //Hook events
-        //startButton.onClick.AddListener(StartGame); // not needed for now since we have multiple scenes to choose from
         optionButton.onClick.AddListener(EnableOption);
         aboutButton.onClick.AddListener(EnableAbout);
-        aboutButton.onClick.AddListener(EnableAbout);
-        aboutButton.onClick.AddListener(EnableExit);
+        exitButton.onClick.AddListener(EnableExit);
 
         foreach (var item in returnButtons)
         {
@@ -36,24 +34,9 @@ public class InGameMenuLoad : MonoBehaviour
         }
     }
 
-    public void QuitGame()
+    public void HideAllMainMenu()
     {
-        // Quit the game in the editor
-#if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
-#endif
-
-        Application.Quit();
-    }
-
-    public void StartGame()
-    {
-        HideAll();
-        SceneTransitionManager.singleton.GoToSceneAsync(1);
-    }
-
-    public void HideAll()
-    {
+        startMainMenu.SetActive(true);
         mainMenu.SetActive(false);
         options.SetActive(false);
         about.SetActive(false);
@@ -61,6 +44,7 @@ public class InGameMenuLoad : MonoBehaviour
 
     public void EnableMainMenu()
     {
+        startMainMenu.SetActive(false);
         mainMenu.SetActive(true);
         options.SetActive(false);
         about.SetActive(false);
@@ -79,6 +63,6 @@ public class InGameMenuLoad : MonoBehaviour
     }
     public void EnableExit()
     {
-        HideAll();
+        HideAllMainMenu();
     }
 }
